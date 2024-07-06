b38718b9774c9e5233c28bce9f42c3a2bac70e7b
Sergey <s.pavluchenko90@gmail.com>
2024-07-06 17:27:57 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,22 @@
+import React, { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
 import { GlobalStyled } from './GlobalStyled';
+import { Layout } from './Layout/Layout';
 
-export const App = () => {
-  return (
-    <div>
-      <GlobalStyled />
-    </div>
-  );
-};
+export class App extends Component {
+  state = { searchText: '' };
+
+  searchValues = text => {
+    console.log(text);
+    this.setState({ searchText: text });
+  };
+
+  render() {
+    return (
+      <Layout>
+        <Searchbar onAnswerText={this.searchValues} />
+        <GlobalStyled />
+      </Layout>
+    );
+  }
+}
